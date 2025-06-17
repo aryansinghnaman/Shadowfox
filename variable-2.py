353294cf9c298810a8027570df8f3d7ea13a98cf
Aryan Singh <aryansinghnaman2@gmail.com>
2025-06-17 12:16:25 +0530

    Add files via upload

---
+++ variable-2.py
@@ -0,0 +1,10 @@
+#2nd question
+
+import keyword
+
+print("List of Python keywords:")
+print(keyword.kwlist)
+
+
+my_variable = 4
+print("Value of my_variable:", my_variable)
---
+++ variable-3.py
@@ -0,0 +1,9 @@
+principal = 5000        
+rate_of_interest = 5    
+time = 3                
+
+# Calculate Simple Interest
+simple_interest = (principal * rate_of_interest * time) / 100
+
+
+print("Simple Interest for", time, "years is:", simple_interest)
